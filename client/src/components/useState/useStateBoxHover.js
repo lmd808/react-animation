cd2b621828f419402a27a5dcbe37c2aab0c24fc5
Laura DiTommaso <52080492+lmd808@users.noreply.github.com>
2020-03-03 11:39:54 -0500

    adding explaination to box Example

--- client/src/components/useState/useStateBoxHover.js
+++ client/src/components/useState/useStateBoxHover.js
@@ -9,21 +9,52 @@ const UseStateBoxExample= (props)=>  {
  
 
   return (
- <div>
-    <div style={{background:'red', width:'300px', height:'200px'}}
+ <div className="container">
+   <div className="row justify-content-center">
+     
+     <div className="col-sm-10 offset-2 justify-content-center">
+     <div style={{background:'red', maxWidth:'600px', height:'200px'}}
       onMouseEnter={() => setHover(true) }
       onMouseLeave={() => setHover(false)}
-      onChange={console.log(hover)}/>
-
+      onChange={console.log(hover)}/>     
+              <h3 >
+                Hover over the box! Monitor the state below: 
+              </h3>
+              <br/>
+              <p>State of hover: {val} </p>
      
-<h3>
-      Hover over the box!
-</h3>
-  <h5>
-      Check The console To Track Changes in State!
-  </h5>
-  <p>State of hover: {val} </p>
-  </div>   )
+        </div> 
+        
+
+     </div>
+     <br/>
+     <div className="row">
+       <div className="col-sm">
+         <h1>What's happening here?</h1>
+         <h5>Extremely Basic monitoring of state</h5>
+
+         <p>In short we are adding React State to our functional component using the useState hook during a onMouseEnter/onMouseLeave event. In this example the state that we are monitoring is called 'hover'</p>
+         <p>
+           Traditionally state would be declared in a class component by accessing the state component and adding new keys to it. However with use state you declare a state variable like the following: 
+           <br/> 
+           <span >const [hover, setHover] = useState(false);</span>
+           <br/>
+           Here we create our state variable (hover), a call back function to set the value of hover thought the component (setHover) and set it the useState hook. The usestate hook takes a single parameter- the initial state. 
+           This state can be be of any data type. useState does NOT need to be an object, but it CAN be an object.
+           
+               
+
+
+           
+         </p>
+
+
+
+
+       </div>
+     </div>
+   </div>
+      )
 }
 
 export default UseStateBoxExample;
